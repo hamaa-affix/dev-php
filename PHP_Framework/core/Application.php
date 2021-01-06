a5749954bad06b239e9de4c15ef252e73ff95066
hamaa-affix <hamaagive1108@gmail.com>
2021-01-06 22:40:00 +0900

    make routerMethod

--- PHP_Framework/core/Application.php
+++ PHP_Framework/core/Application.php
@@ -1,10 +1,29 @@
 <?php
+namespace app\core;
+
+use Request;
+
+/**
+ * Class Application
+ *
+ * @author hamaa-affix
+ * @package app/core
+ */
 
 class Application
 {
   public Router $router;
+  public Request $request;
+
   public function __construct()
   {
-    $this->router = new Router();
+    $this->request = new Request();
+    $this->router = new Router($this->request);
   }
+
+  public function run()
+  {
+    $this->router->resolve();
+  }
+
 }
--- PHP_Framework/index.php
+++ PHP_Framework/index.php
@@ -1,9 +1,17 @@
 <?php
 
-$app = new Applcation();
+require_once __DIR__.'./vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application();
 
 $app->router->get('/', function (){
   return 'Hello world';
+});
+
+$app->router->get('/contact', function () {
+  return 'Contact';
 });
 
 $app->run();
